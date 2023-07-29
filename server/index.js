f590d18ea35d527f62c24e4330452ac8036c16d4
Birjusingh Thakur <124129897+BirjuThakur@users.noreply.github.com>
2023-07-29 19:05:23 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -1,42 +1,54 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8000 ;
+const port = process.env.PORT || 8000;
 const cors = require("cors");
+const http = require("http");
 const socket = require("socket.io");
 
+const chatMembers = new Map();
+
 app.use(cors({
-    origin:"http://127.0.0.1:3000"
+  origin: "http://127.0.0.1:3000"
 }));
 
-app.get("/",(req,res)=>{
-    res.send("welcome")
-})
-
-const server = app.listen(port,()=>{
-    console.log(`running on port ${port}`)
-})
-
-//use socket 
-const io = socket(server,{
-    cors:{
-        origin:"*"
-    }
+app.get("/", (req, res) => {
+  res.send("Welcome to the chat server");
 });
 
-io.on("connection",(socket)=>{
-    console.log(socket.id);
-    socket.on("room_join",(data)=>{
-        socket.join(data)
-        console.log("join room ",data)
-    })
-    // it sends data is in object {room:"", nama:"", message:""}
-    socket.on("send_message",(data)=>{
-        console.log("message getting",data)
-        socket.to(data.room).emit("recieve_message",data)
-    })
-    
-    //user disconnected
-    socket.on("disconnect",()=>{
-        console.log("user disconnect")
-    })
-})
+const server = http.createServer(app);
+
+const io = socket(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", (socket) => {
+  // Function to update chat members list and emit it to all clients
+  const updateChatMembers = () => {
+    io.emit("chat_members", Array.from(chatMembers.values()));
+  };
+
+  socket.on("room_join", (data) => {
+    chatMembers.set(socket.id, data.name);
+    updateChatMembers();
+    socket.join(data.room);
+    console.log(`User ${data.name} joined room ${data.room}`);
+  });
+
+  socket.on("send_message", (data) => {
+    console.log("message getting", data);
+    io.to(data.room).emit("recieve_message", data);
+  });
+
+  
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    chatMembers.delete(socket.id);
+    updateChatMembers();
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
